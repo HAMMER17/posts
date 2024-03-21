2b5a22cf856165f0617718cd4e8e4dbdbe8963a7
HAMMER17 <plutalov70@gmail.com>
2024-03-21 19:38:08 +0300

    button

--- src/components/CardPost.tsx
+++ src/components/CardPost.tsx
@@ -6,14 +6,14 @@ import { PostType } from "../types/PostType"
 const CardPost = ({ id, title, body }: PostType) => {
 
   return (
-    <Link to={`/post/${id}`} >
-      <div className="card_post" key={id}>
-        <div style={{ color: 'red' }}>{id}</div>
-        <h3>{title}</h3>
-        <p>{body}</p>
-        <button className="btn"><Link to={`/post/${id}`} >open</Link></button>
-      </div>
-    </Link>
+
+    <div className="card_post" key={id}>
+      <div style={{ color: 'red' }}>{id}</div>
+      <h3>{title}</h3>
+      <p>{body}</p>
+      <Link to={`/post/${id}`} ><button className="btn">open</button></Link>
+    </div>
+
   )
 }
 
